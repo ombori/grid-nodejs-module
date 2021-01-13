5c4de063f1058aba4f7b662ccbc277e2751c7d8b
Alexey Guskov <lexag@mail.ru>
2021-01-13 13:08:48 +0100

    feat: add m2m and m2a examples

--- schema.ts
+++ schema.ts
@@ -4,5 +4,11 @@ export type Settings = { enabled: false } | {
    */
   enabled: true;
 
+  /**
+   * @title A test setting
+   * @default "Default value"
+   */
+  test_setting: string;
+
   // TODO: add your settings here
 }
--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const { Mqtt } = require("azure-iot-device-mqtt");
 const { ModuleClient } = require("azure-iot-device");
 
+const { IOTEDGE_DEVICEID, MODULENAME_TEST_SETTING } = process.env;
+
+if (!IOTEDGE_DEVICEID) {
+  console.error('IOTEDGE_DEVICEID not set, m2a and m2m broadcasts will not work');
+}
+
 const delay = n => new Promise(resolve => setTimeout(resolve, n));
+
+async function processTestMessage() {
+  // TODO: process Test.Message
+}
+
 async function main() {
   const client = await ModuleClient.fromEnvironment(Mqtt.Transport);
 
@@ -19,7 +30,23 @@ async function main() {
   }
   console.log("IoT Hub module client initialized");
 
-  // insert your code here
+  const broadcast = (payload) => {
+    if (!IOTEDGE_DEVICEID) { return };
+    return client.invokeMethod(IOTEDGE_DEVICEID, 'GdmAgent', {
+      methodName: 'broadcast',
+      payload
+    });
+  }
+
+  // TODO: insert your code here
+  console.log('test_setting value is', MODULENAME_TEST_SETTING);
+
+  // In this example we send TestModule.Event message every second
+  let seq = 0;
+  setInterval(() => {
+    broadcast({ type: 'TestModule.Event', some: 'data', seq });
+    seq += 1;
+  }, 1000);
 
   client.onMethod('restart', async (req, res) => {
     console.log('Restart command received, exiting');
@@ -27,9 +54,26 @@ async function main() {
     setInterval(() => process.exit(0), 1000);
   });
 
+  // TODO: insert your methods here
+
   client.onMethod('broadcast', async (req, res) => {
-    console.log('recv broadcast', req.payload);
-    res.send(200, 'ok');
+    try {
+      const data = JSON.parse(req.payload);
+      console.log('recv broadcast', data);
+
+      switch (data.type) {
+        case "Test.Message":
+          processTestMessage(data);
+          break;
+
+        // TODO: insert your message handlers here
+      }
+
+      res.send(200, 'ok');
+    } catch (e) {
+      console.error('Cannot process broadcast', e.toString());
+      res.send(500, 'error');
+    }
   });
 }
 
